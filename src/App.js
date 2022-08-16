e86ccf82c164488b859d594302c3768e6bf57423
Abhisheq04 <abhishekst@techis.io>
2022-08-16 15:08:03 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./Navbar";
-import {useState, createContext} from "react"; 
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-export const AppContext = createContext();
+import { useState } from "react";
+import { Task } from "./Task";
 
 function App() {
-  const [username, setUsername] = useState("PedroTech");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+      completed: false,
+    };
+    setTodoList(task.taskName !== "" ? [...todoList, task] : todoList);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-        <AppContext.Provider value={{username, setUsername}}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
-        </AppContext.Provider>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              completed={task.completed}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import {useContext} from "react";
-import {AppContext} from '../App';
-
-export const Home = () => {
-    const {username} = useContext(AppContext)
-  return <h1> THIS IS THE HOME PAGE and user is : {username}</h1>;
-};
